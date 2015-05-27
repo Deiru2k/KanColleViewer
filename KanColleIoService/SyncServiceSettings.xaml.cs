c060aa888262008347d803608ca958fa05ad753f
Oleg Evseev <ixen.eoa@me.com>
2015-05-27 18:33:58 +0300

    Settings tab

---
+++ KanColleIoService/SyncServiceSettings.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KanColleIoService
+{
+    /// <summary>
+    /// Логика взаимодействия для SyncServiceSettings.xaml
+    /// </summary>
+    public partial class SyncServiceSettings : UserControl
+    {
+        public SyncServiceSettings()
+        {
+            InitializeComponent();
+        }
+
+        private async void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await SyncService.Current.LogIn(Username.Text, Password.Password);
+
+                Username.IsEnabled = false;
+                Password.IsEnabled = false;
+
+                Button logInButton = sender as Button;
+                logInButton.IsEnabled = false;
+                logInButton.Content = "Success";
+            }
+            catch (APIException ex)
+            {
+                SyncService.Current.ReportException(ex);
+            }
+        }
+    }
+}
--- KanColleIoService/UserInterfaceHelpers.cs
+++ KanColleIoService/UserInterfaceHelpers.cs
@@ -12,9 +12,24 @@ namespace KanColleIoService
     {
         private static ContentPresenter GetContentPresenter(Window mainWindow)
         {
-            // My skill of WPF sucks.
+            // My WPF skills suck.
             ContentControl contentControl = ((mainWindow.Content as Grid).Children[1] as Grid).Children[1] as ContentControl;
             return VisualTreeHelper.GetChild(contentControl, 0) as ContentPresenter;
+        }
+
+        private static TabItem ConstructTabItem()
+        {
+            TextBlock headerTextBlock = new TextBlock();
+            headerTextBlock.Text = "KanColle.io";
+            headerTextBlock.Foreground = Brushes.LightGray;
+            headerTextBlock.Margin = new Thickness(12, 0, 12, 0);
+            headerTextBlock.FontSize = 14;
+
+            TabItem tabItem = new TabItem();
+            tabItem.Header = headerTextBlock;
+            tabItem.Content = new SyncServiceSettings();
+
+            return tabItem;
         }
 
         /// <summary>
@@ -25,6 +40,10 @@ namespace KanColleIoService
         {
             ContentPresenter contentPresenter = GetContentPresenter(mainWindow);
             StartContent startContent = VisualTreeHelper.GetChild(contentPresenter, 0) as StartContent;
+
+            // And it should've been much easier if the controls were named. Welp.
+            TabControl tabControl = ((((startContent.Content as Grid).Children[1] as Grid).Children[2] as Border).Child as ScrollViewer).Content as TabControl;
+            tabControl.Items.Add(ConstructTabItem());
         }
 
         /// <summary>
@@ -34,7 +53,7 @@ namespace KanColleIoService
         public static void AddSettingsTabMain(Window mainWindow)
         {
             ContentPresenter contentPresenter = GetContentPresenter(mainWindow);
-            MainContent mainContent = VisualTreeHelper.GetChild(contentPresenter, 1) as MainContent;
+            MainContent mainContent = VisualTreeHelper.GetChild(contentPresenter, 0) as MainContent;
         }
     }
 }
